848ba1aa966b6dfb70d4e157af80dc3ef30b4a59
Miguel Angel Rubio <23417202+MikeRubio@users.noreply.github.com>
2023-08-30 11:05:06 +0200

    Netlify deployment (#14)
    * feat: setup netlify deployment
    
    * feat: setup netlify deployment
    
    * feat: setup netlify deployment

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import serverless from "serverless-http";
 
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 
-import { typeDefs, resolvers } from "../src/graphql";
-import charactersRouter from "../src/routes/characters";
-import characterRouter from "../src/routes/character";
+import { typeDefs, resolvers } from "./graphql";
+import charactersRouter from "./routes/characters";
+import characterRouter from "./routes/character";
 
 dotenv.config();
 const app = express();
@@ -25,17 +26,17 @@ const bootstrapServer = async () => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
-  app.use("/images", express.static("public"));
+  app.use("/.netlify/functions/images", express.static("public"));
   app.use(
-    "/graphql",
+    "/.netlify/functions/graphql",
     expressMiddleware(server, {
       context: async ({ req, res }) => ({
         url: `${req.protocol}://${req.get("host")}`,
       }),
     })
   );
-  app.use("/characters", charactersRouter);
-  app.use("/character", characterRouter);
+  app.use("/.netlify/functions/characters", charactersRouter);
+  app.use("/.netlify/functions/character", characterRouter);
 
   app.listen(port, () => {
     console.log(`🚀 Express running at: http://localhost:${port}`);
@@ -44,3 +45,4 @@ const bootstrapServer = async () => {
 };
 
 bootstrapServer();
+export const handler = serverless(app);
